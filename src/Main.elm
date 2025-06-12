134ec8368edcea33b671b376b8da519e9333168f
MrBocch <jorgealberto1436@gmail.com>
2025-06-12 09:38:29 -0600

    elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -1,16 +1,16 @@
 module Main exposing (..)
 
 import Browser
-
 import Html exposing (..)
 import Html.Attributes exposing (..)
-
-import Http
-import Json.Decode as Decode
-import Http exposing (expectJson)
 import Html.Events exposing (onClick)
+import Http exposing (expectJson)
+import Json.Decode as Decode
 
-baseUrl = "https://api.quotable.io/"
+
+baseUrl =
+    "https://api.quotable.io/"
+
 
 main =
     Browser.element
@@ -21,13 +21,18 @@ main =
         }
 
 
+
 -- msg
+
+
 type Msg
     = GetQuote
     | LoadQuote (Result Http.Error Quote)
 
 
+
 -- Model
+
 
 type alias Quote =
     { content : String
@@ -35,23 +40,26 @@ type alias Quote =
     , tags : List String
     }
 
+
 type alias Model =
     Quote
 
-initModel :Model
+
+initModel : Model
 initModel =
     quote1
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
     ( initModel
     , fetchQuote
     )
 
+
 fetchQuote =
     Http.get
-        { url = (baseUrl ++ "random")
+        { url = baseUrl ++ "random"
         , expect = Http.expectJson LoadQuote quoteDecoder
         }
 
@@ -60,48 +68,65 @@ quote1 : Quote
 quote1 =
     { content = "El mas callado es el que menos habla"
     , author = "nose"
-    , tags = ["nose", "nose"]
+    , tags = [ "nose", "nose" ]
     }
+
 
 cantLoad : Quote
 cantLoad =
     { content = "Cant load in data ;("
     , author = "sorry"
-    , tags = ["nose", "nose"]
+    , tags = [ "nose", "nose" ]
     }
 
 
+
 -- VIEW
+
 
 view : Model -> Html Msg
 view model =
     main_ []
         [ div [ class "quote-box" ]
             [ h1 [ class "quote" ] [ text model.content ]
-            , h3 [ class "author" ] [text ("~ " ++ model.author) ]
+            , h3 [ class "author" ] [ text ("~ " ++ model.author) ]
             ]
         , button [ onClick GetQuote ] [ text "next" ]
         ]
 
 
+
 -- update
-update : Msg -> Model -> (Model, Cmd Msg)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         LoadQuote resp ->
             case resp of
-                Ok quote -> (quote, Cmd.none)
-                Err res ->  (cantLoad, Cmd.none)
+                Ok quote ->
+                    ( quote, Cmd.none )
+
+                Err res ->
+                    ( cantLoad, Cmd.none )
+
         GetQuote ->
-            (model, fetchQuote)
+            ( model, fetchQuote )
+
 
 
 -- subscriptions (so what is this for?)
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+
+
 -- JSON
+
+
 quoteDecoder =
     Decode.map3 Quote
         (Decode.field "content" Decode.string)
